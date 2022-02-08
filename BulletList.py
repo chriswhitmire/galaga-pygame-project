ce72284f52c3911b3b31dfdb6d7d786162f10822
chriswhitmire <chriswhitmire94@gmail.com>
2022-02-08 00:01:04 -0500

    Started defining class and its constructor and collision function

--- BulletList.py
+++ BulletList.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import boolean
 import pygame
 
 from Bullet import Bullet
@@ -6,4 +7,16 @@ class BulletList:
 
     # Constructor
     def __init__(self) -> None:
+        # list of bullets to store
+        self.bulletList = []
+        
+        pass
+
+
+    def checkCollision(self, aRect) -> boolean:
+        """Returns true if the bullet is colliding with given aRect. Otherwise, returns false
+
+        Args:
+            aRect (pygame rect): hitbox of object to check collision against
+        """
         pass
